1b2e9573cf77d0e2f3213680296d90e9f3e69964
Caio Goncalves <caiorhcp12@gmail.com>
2024-08-27 16:06:29 -0300

    fix tranfer_service print

--- lib/services/transfer_service.dart
+++ lib/services/transfer_service.dart
@@ -2,12 +2,15 @@ import 'package:dart_bank/models/account.dart';
 
 void transferService(Account origin, Account destination, double value) {
   if(origin == destination){
+     print('Erro: Não é possivel transferir da mesma conta');
     throw ArgumentError('Não foi possivel a transferencia');
   }
   if(value <= 0 ) {
+     print('Erro: Não é possivel transferir, saldo insuficiente');
   throw ArgumentError('Saldo Insuficiente');
   }
   if(value > origin.getBalance()){
+    print('Erro: Não é possivel transferir valor maior que o saldo atual');
     throw ArgumentError('Erro: Valor de transferencia maior que o saldo');
   }
   origin.removeBalance(value);
